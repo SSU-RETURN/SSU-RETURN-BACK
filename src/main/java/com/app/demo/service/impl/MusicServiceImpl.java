aa2d3132644e570b2c03c7e243e90bf761dc41c9
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2024-05-02 16:41:44 +0900

    🪓[fix]: 저장 후 리턴 값 수정
    - 저장 API 호출 후 이미 db에 있는 음악 정보도 반환하게 수정

--- src/main/java/com/app/demo/service/impl/MusicServiceImpl.java
+++ src/main/java/com/app/demo/service/impl/MusicServiceImpl.java
@@ -95,6 +95,8 @@ public class MusicServiceImpl implements MusicService {
 
                 musicRepository.save(music);
                 savedMusics.add(music);
+            }else{
+                savedMusics.add(existingMusic.get());
             }
         }
 
