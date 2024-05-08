7b937fc66c4a2ba537caf52e552c4d48f6c8c89a
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2024-05-08 22:48:32 +0900

    🪓 [fix]: 빌드 오류
    - SecurityConfig.java 수정

--- src/main/java/com/app/demo/config/SecurityConfig.java
+++ src/main/java/com/app/demo/config/SecurityConfig.java
@@ -8,19 +8,44 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+/*
+    private final JwtTokenProvider jwtTokenProvider;
+    public SecurityConfig(JwtTokenProvider jwtTokenProvider){
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+*/
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())  // CSRF 보호 비활성화
-                .authorizeHttpRequests((authz) -> authz
-                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/**").permitAll()  // Swagger UI 접근 허용
-                        .anyRequest().authenticated());  // 나머지 요청은 인증 필요
+                .authorizeHttpRequests((auth)-> auth
+                        .requestMatchers("/login","/join","joinProc","/swagger-ui/**", "/v3/api-docs/**", "/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")  // 로그인 페이지
+                        .loginProcessingUrl("/loginProc")  // 로그인 처리 URL
+                        .defaultSuccessUrl("/", true)  // 로그인 성공 시 리디렉션 경로
+                        .permitAll()  // 모든 사용자가 로그인 페이지 접근 허용
+                )
+                .logout((logout) -> logout
+                        .logoutUrl("/logout")  // 로그아웃 처리 URL
+                        .logoutSuccessUrl("/login?logout")  // 로그아웃 성공 후 리디렉션 경로
+                        .permitAll()  // 로그아웃 URL 접근 허용
+                )
+                .csrf(csrf -> csrf.disable()); // CSRF 보호 비활성화
+
         return http.build();
     }
 }
