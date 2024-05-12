69a1a6deefe692cb2e906c2731b55b9edc838c2f
csh0924 <csh000924@gmail.com>
2024-05-13 01:20:44 +0900

    :axe:[Fix]AIPlaylist 주소 2차수정
    소문자만 사용

--- src/main/java/com/app/demo/controller/AIPlaylistController.java
+++ src/main/java/com/app/demo/controller/AIPlaylistController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/aiPlaylist")
+@RequestMapping("/api/aiplaylist")
 public class AIPlaylistController {
 
     private final AIPlaylistService aiPlaylistService;
