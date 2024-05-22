735f600ee4ab907f1b9de77dc16dc7f49c142bf8
csh0924 <csh000924@gmail.com>
2024-05-22 23:24:21 +0900

    :axe:[Fix]: PreferenceAPI수정
    repository에 findByMember추가

--- src/main/java/com/app/demo/repository/MemberPreferenceRepository.java
+++ src/main/java/com/app/demo/repository/MemberPreferenceRepository.java
@@ -1,10 +1,12 @@
 package com.app.demo.repository;
 
 
+import com.app.demo.entity.Member;
 import com.app.demo.entity.MemberPreference;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberPreferenceRepository extends JpaRepository<MemberPreference, Long> {
+    MemberPreference findByMember(Member member);
 }
