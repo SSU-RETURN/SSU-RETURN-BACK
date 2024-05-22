787e805cba811e1befba117872e49b1ac4601ca0
csh0924 <87259867+csh0924@users.noreply.github.com>
2024-05-22 23:18:33 +0900

    🌟 [update]: 취향 api 수정 (#52)
    * :axe:[Fix]: Preference 오타 수정
    
    exited->excited
    
    * :axe:[Fix]: Preference API 수정
    
    updatePreference, getPreference의 경로를 memberPreferenceId-> memberId로 수정

--- src/main/java/com/app/demo/controller/MemberPreferenceController.java
+++ src/main/java/com/app/demo/controller/MemberPreferenceController.java
@@ -47,8 +47,8 @@ public class MemberPreferenceController {
     @Operation(summary = "취향", description = "취향조회 API입니다")
     @ApiResponses({@ApiResponse(responseCode = "COMMON200", description = "조회성공")})
     @GetMapping("/details/{memberPreferenceId}")
-    public BaseResponse<MemberPreferenceResponseDTO.MemberPreferenceContentDTO> getMemberPreference(@PathVariable Long memberPreferenceId){
-        MemberPreference memberPreference = memberPreferenceService.getMemberPreference(memberPreferenceId);
+    public BaseResponse<MemberPreferenceResponseDTO.MemberPreferenceContentDTO> getMemberPreference(@PathVariable Long memberId){
+        MemberPreference memberPreference = memberPreferenceService.getMemberPreferenceByMemberId(memberId);
         MemberPreferenceResponseDTO.MemberPreferenceContentDTO responseDTO = MemberPreferenceConverter.toMemberPreferenceContentDTO(memberPreference);
         return BaseResponse.onSuccess(responseDTO);
     }
