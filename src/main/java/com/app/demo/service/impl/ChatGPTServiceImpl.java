d643d1deff3d15ef247564b3606ae29464b5461b
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2024-05-26 06:57:37 +0900

    :star2: [update]: chatGPT 음악 반환 갯수 수정
    - 20개 반환하도록 수정

--- src/main/java/com/app/demo/service/impl/ChatGPTServiceImpl.java
+++ src/main/java/com/app/demo/service/impl/ChatGPTServiceImpl.java
@@ -50,7 +50,7 @@ public class ChatGPTServiceImpl implements ChatGPTService {
     public String createPrompt(String tagString, String tagStringEmotion, String preferString) {
         String prompt = "";
         prompt = String.format(
-                "제가 '%s' 감정을 느끼고 있을 때 들으면 좋은 노래가 있을까요? 저는 그 감정을 느낄 때 평소에 '%s'한 음악을 즐겨 듣습니다. '%s'의 음악을 즐깁니다. '%s' 보다는 '%s'를 조금 더 중점으로 하여 좋은 음악 5개 추천 부탁드립니다. 되도록 한국 음악으로 추천해주세요. 노래는 아래의 형식으로 추천해주세요 (노래 하나의 정보는 { } 묶습니다. 줄바꿈 없이) : [{'가수명', '노래제목'},{'가수명', '노래제목'}, ... ,{'가수명', '노래제목'},{'가수명', '노래제목'}]",
+                "제가 '%s' 감정을 느끼고 있을 때 들으면 좋은 노래가 있을까요? 저는 그 감정을 느낄 때 평소에 '%s'한 음악을 즐겨 듣습니다. '%s'의 음악을 즐깁니다. '%s' 보다는 '%s'를 조금 더 중점으로 하여 좋은 음악 20개 추천 부탁드립니다. 되도록 한국 음악으로 추천해주세요. 노래는 아래의 형식으로 추천해주세요 (노래 하나의 정보는 { } 묶습니다. 줄바꿈 없이) : [{'가수명', '노래제목'},{'가수명', '노래제목'}, ... ,{'가수명', '노래제목'},{'가수명', '노래제목'}]",
                 tagString, tagStringEmotion, preferString, preferString, tagString);
         return prompt;
     }
